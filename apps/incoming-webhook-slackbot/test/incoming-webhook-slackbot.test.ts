2d2a0a8778051d5585656da08f8678e4658582c0
shunppe <70781900+shunppe@users.noreply.github.com>
2022-09-26 00:44:00 +0900

    init incoming-webhook app

---
+++ apps/incoming-webhook-slackbot/test/incoming-webhook-slackbot.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as IncomingWebhookSlackbot from '../lib/incoming-webhook-slackbot-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/incoming-webhook-slackbot-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new IncomingWebhookSlackbot.IncomingWebhookSlackbotStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
